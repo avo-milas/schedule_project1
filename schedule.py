3f5237ed7b38f1259a66dbe8a85a44238ac5283a
avo-milas <avo_milas@mail.ru>
2021-09-23 02:03:04 +0400

    внесены все предметы (все комплектующие)

---
+++ schedule.py
@@ -0,0 +1,11 @@
+data = {'Русский язык': [['пн 10:00', 'вт 11:40'], 'К.А. Патрикеева', 'https://us02web.zoom.us/j/86754495063?pwd=STlaaCtKSVE', '1984'],
+        'Экономика1': [['пн 11:40', 'ср 13:50'], 'С.В. Савицкий', 'https://vk.com/away.php?to=https%3A%2F%2Fus02web.zoom.us%2Fj%2F9302461843%3Fpwd%3DQThHakJoTVhiY2gwYnNWMWV5Qkw4QT09&cc_key=', '-'],
+        'Экономика2': [['пн 13:50', 'пт 11:40', 'пт 13:50'], 'М.А. Грязнов', 'https://vk.com/away.php?to=https%3A%2F%2Fus02web.zoom.us%2Fj%2F87659156044%3Fpwd%3DUHcwbzNxS2RhU21hWnlRV1U3VDlGdz09', '-'],
+        'Математика': [['пн 15:30', 'ср 10:00', 'ср 11:40', 'пт 10:00'], 'А.Б. Акимов', 'https://us02web.zoom.us/j/3515530970', '-'],
+        'Обществознание': [['вт 10:00'], 'А.Р. Рахматуллин', 'https://us02web.zoom.us/j/2283570012?pwd=WXBsZVY1eG5SZTVvSENnRzZ2cmhtUT09', '-'],
+        'Английский язык': [['вт 13:50', 'пт 15:30'], 'Т.Н. Звездилина', 'https://vk.com/away.php?to=https%3A%2F%2Fus02web.zoom.us%2Fj%2F2734958230%3Fpwd%3DeS9DSjYrS2hLQ2d1SU9yUHA2OUhyZz09&cc_key=', '-'],
+        'Литература': [['ср 15:30'], 'А.Ю. Денисов', 'https://us04web.zoom.us/j/2899413065?pwd=N2VCVTRhYWFB', 'N51ak7'],
+        'История': [['вт 15:30'], 'О.П. Пономаренко', 'https://us02web.zoom.us/j/5187163321', '-'],
+        'Информатика': [['11:40'], 'М.С. Густокашин', 'https://vk.com/away.php?to=https%3A%2F%2Fus04web.zoom.us%2Fj%2F77144787511%3Fpwd%3DMENKVFBwNXU&cc_key=', '-']}
+
+print(f'Предмет: {}\n Препод: {}\n Ссылка: ')
